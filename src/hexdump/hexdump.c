f739d12da45f51a5d020508a825e23e9cb5ce97c
omni-works <83828882+omnit3a@users.noreply.github.com>
2023-01-15 10:32:54 -0500

    Fix some bugs and remove `textdump`
    Fixed bug where segfault would occur upon provide only a flag to `list`, and also removed `textdump` and added `-tx` and `-oc` flags to `hexdump`, which list contents of a file as text and as octal respectively

--- src/hexdump/hexdump.c
+++ src/hexdump/hexdump.c
@@ -1,22 +1,62 @@
 #include <stdio.h>
+#include <string.h>
+
+#define TEXT_DUMP_FLAG "-tx"
+#define OCTAL_DUMP_FLAG "-oc" 
 
 int main(int argc, char ** argv){
-	if (argc == 1){
+	
+	char * filename;
+
+	int foundTxFlag = 0;
+	int foundOcFlag = 0;
+
+	if (argc <= 1){
 		fprintf(stderr, "read: must provide filename\n");
 		return 1;
+	} else if (argc > 1 && argc < 4){
+		
+		if (argc == 3){
+			for (int i = 1 ; i < argc+1 ; i++){
+				if (strcmp(argv[i], TEXT_DUMP_FLAG) == 0){
+					foundTxFlag = 1;
+				} else if (strcmp(argv[i], OCTAL_DUMP_FLAG) == 0){
+					foundOcFlag = 1;
+				} else {
+					filename = argv[i];
+				}
+				if (i == 2){
+					break;
+				}
+			}
+		} else {
+			filename = argv[1];
+		}
+
 	}
 
 	FILE * tempFile;
 	char tempCh;
-
-	tempFile = fopen(argv[1], "r");
 	
+	if (filename){
+		tempFile = fopen(filename, "r");
+	}
+
 	if (tempFile){
 		while ((tempCh = getc(tempFile)) != EOF){
-			if (tempCh < 0x10){
-				printf("0");
+			if (foundTxFlag){
+				printf("%c",tempCh);
+			} else if (foundOcFlag){
+				printf("%o ",tempCh);
+			} else {
+				if (tempCh < 0x10){
+					printf("0");
+				}
+				printf("%X ",tempCh);
 			}
-			printf("%X ",tempCh);
+		}
+		if (!foundTxFlag){
+			printf("\n");
 		}
 		fclose(tempFile);
 	} else {
